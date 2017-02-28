672caad4efe334294d162bee0dce3b07bcf190b8
TsaiChiaWen <amytsai17@hotmail.com>
2017-02-28 22:03:19 +0800

    Unity pj to connect with PC & w/ 2 VR robot

---
+++ Unity_mouse/Assets/Client_Recv.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+//using System.Text;
+//using System.Threading;
+using System.IO;
+
+public class Client_Recv
+{
+	public TcpClient tc = null;
+	public bool co_ready = false;
+	public byte[] data;
+	public string recvMsg;
+
+	public void Run ()
+	{
+		this.tc = new TcpClient ();  
+		this.tc.Connect ("127.0.0.1", 5566);  //if use "try-catch": 等待連線，若未連線則會停在這行?
+		data = new byte[this.tc.ReceiveBufferSize];  
+		this.tc.GetStream ().BeginRead (data, 0, System.Convert.ToInt32 (this.tc.ReceiveBufferSize), ReceiveMessage, null);
+		co_ready = true;
+		recvMsg = null; //!?
+
+	}		
+	public void ReceiveMessage (IAsyncResult ar)
+	{  
+		try 
+		{
+			int bytesRead;
+			bytesRead = this.tc.GetStream ().EndRead (ar);
+
+			if (bytesRead < 1) 
+			{  
+				return;  
+			} 
+			else 
+			{  
+				recvMsg = System.Text.Encoding.ASCII.GetString (data, 0, bytesRead);
+			}  
+			this.tc.GetStream ().BeginRead (data, 0, System.Convert.ToInt32 (this.tc.ReceiveBufferSize), ReceiveMessage, null); 
+		} 
+		catch (Exception) 
+		{  
+
+		}
+
+	}
+}
---
+++ Unity_mouse/Assets/Client_Send.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+//using System.Text;
+//using System.Threading;
+using System.IO;
+
+public class Client_Send
+{
+	TcpClient tc = null;
+	NetworkStream ns = null;
+	StreamWriter sw = null;
+
+	public void Run ()
+	{
+		this.tc = new TcpClient ();  
+		this.tc.Connect ("127.0.0.1", 5566);  //if use "try-catch": 等待連線，若未連線則會停在這行?
+
+		ns = tc.GetStream ();
+		sw = new StreamWriter (ns);
+	}		
+	public void Send (string sendMsg)  //傳送訊息
+	{	
+		if (sendMsg.Length < 50)  //7*6+5+10=51 (6 joints * '(-)0.0000', round off to the 4th decimal + ',-999.9999' )
+			throw new NullReferenceException("ERROR: 'targetAngle[1~6]' send to PC go wrong!");
+		else if (ns != null) {
+			//sw = new StreamWriter (ns);
+			sw.Write (sendMsg);
+			sw.Flush ();
+		}
+	}
+}
